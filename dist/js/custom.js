c1ddca6a364195975b9e8da111529ca331f14e27
mohamedmansy316 <mohamedmansy855@gmail.com>
2021-10-22 22:10:44 +0200

    add first time

---
+++ dist/js/custom.js
@@ -0,0 +1,27 @@
+// let switcherLis = document.querySelectorAll(".switcher li");
+// let imgs = document.querySelectorAll(".gallery img");
+// let defaultActive = document.querySelector(".active");
+// switcherLis.forEach((li) => {
+//   li.addEventListener("click", removeActive);
+//   li.addEventListener("click", manageImgs);
+// });
+
+// // Remove Active Class From All Lis And Add To Current
+// function removeActive() {
+//   switcherLis.forEach((li) => {
+//     li.classList.remove("active");
+//     this.classList.add("active");
+//   });
+// }
+
+// // Manage Imgs
+// function manageImgs() {
+//   imgs.forEach((img) => {
+//     img.style.display = "none";
+//   });
+//   document.querySelectorAll(this.dataset.cat).forEach((el) => {
+//     el.style.display = "block";
+//   });
+// }
+
+
